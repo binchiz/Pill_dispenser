c72edc018b54c902b687e91eb5f4f01a8ba2efa9
Zhou Binchi <zbinchi@outlook.com>
2024-12-07 14:54:17 +0200

    'merged' lora feature

--- src/drivers/lora.c
+++
@@ -1,9 +0,0 @@
-#include "lora.h"
-#include "hardware/gpio.h"
-#include "hardware/uart.h"
-
-void setup_lora(const lora_t *lora) {
-    uart_init(lora->uart, lora->baudrate);
-    gpio_set_function(lora->tx_pin, GPIO_FUNC_UART);
-    gpio_set_function(lora->tx_pin, GPIO_FUNC_UART);
-}
