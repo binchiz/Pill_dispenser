47930564f93cde818001bddd82feb70bfb1362a4
Sheng Tai <alxt9@outlook.com>
2024-12-09 03:52:41 +0200

    updated debug printing format, disable compilation when ENABLE_DPRINT set to 0

--- src/utils/debug.c
+++ src/utils/debug.c
@@ -4,20 +4,38 @@
 
 static int current_debug_level = DEBUG_LEVEL_NONE;
 
-void set_debug_level(int debug_level)
-{
-    current_debug_level = debug_level;
-}
-int dprintf(int debug_level, const char *fmt, ...)
-{
-    if (debug_level > current_debug_level)
-    {
+void set_debug_level(int debug_level) { current_debug_level = debug_level; }
+
+int dprintf(int debug_level, const char *fmt, ...) {
+#ifdef ENABLE_DPRINT
+    if (debug_level > current_debug_level) {
         return 0;
     }
     va_list args;
     va_start(args, fmt);
-    fprintf(stderr, "[DBG%d] ", debug_level);
+    char prefix[16];
+    switch (debug_level) {
+    case DEBUG_LEVEL_ERROR:
+        sprintf(prefix, "ERROR");
+        break;
+    case DEBUG_LEVEL_WARN:
+        sprintf(prefix, "WARN");
+        break;
+    case DEBUG_LEVEL_INFO:
+        sprintf(prefix, "INFO");
+        break;
+    case DEBUG_LEVEL_DEBUG:
+        sprintf(prefix, "DEBUG");
+        break;
+    default:
+        sprintf(prefix, "UNKNOWN");
+        break;
+    }
+    fprintf(stderr, "[%s] ", prefix);
     int ret = vfprintf(stderr, fmt, args);
     va_end(args);
     return ret;
+#else
+    return 0;
+#endif
 }
