985304601868dc9aea836ac99be314a4bd14c3ac
Sheng Tai <alxt9@outlook.com>
2024-12-09 16:37:27 +0200

    added dprint test

---
+++ test/test_debug_msg.c
@@ -0,0 +1,12 @@
+#include "pico/stdlib.h"
+#include "utils/debug.h"
+
+int main(void) {
+    stdio_init_all();
+    set_debug_level(DEBUG_LEVEL_DEBUG);
+    dprintf(DEBUG_LEVEL_DEBUG, "Test msg: debug level: debug\n");
+    dprintf(DEBUG_LEVEL_INFO, "Test msg: debug level: info\n");
+    dprintf(DEBUG_LEVEL_WARN, "Test msg: debug level: warn\n");
+    dprintf(DEBUG_LEVEL_ERROR, "Test msg: debug level: error\n");
+    return 0;
+}
