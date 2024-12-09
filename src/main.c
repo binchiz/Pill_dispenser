7e7d3aeb50af0626d485eb8d804b9521fcb11bd0
Zhou Binchi <zbinchi@outlook.com>
2024-12-09 13:37:13 +0200

    implemented main with state machine

--- src/main.c
+++ src/main.c
@@ -1,20 +1,30 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "utils/debug.h"
+#include "drivers/led.h"
 #include "buttons.h"
 #include "dispenser_control.h"
 #include "lora.h"
-#include "pico/stdlib.h"
 #include "storage.h"
-#include "utils/debug.h"
-#include <stdio.h>
+#include "pill_dispenser_sm.h"
+
 
 int main(void) {
     stdio_init_all();
     set_debug_level(DEBUG_LEVEL_DEBUG);
-
+    init_led();
     init_storage();
     init_dispenser();
     init_buttons();
+    enable_buttons();
     lora_init_and_connect();
 
-    printf("Hello from main!\n");
+    dispenser_sm dsm = {stStart};
+    //send_message(BOOT, "Dispenser Boot");
+
+    while (true) {
+        run_dispenser_sm(&dsm);
+        sleep_ms(500);
+    }
     return 0;
 }
